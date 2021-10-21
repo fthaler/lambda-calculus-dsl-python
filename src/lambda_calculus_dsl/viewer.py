9d0dac082a5f09b86c0221771b5b9c73bd2a0119
Felix Thaler <thaler@cscs.ch>
2021-10-21 08:54:13 +0200

    Updated viewer

--- src/lambda_calculus_dsl/viewer.py
+++ src/lambda_calculus_dsl/viewer.py
@@ -1,65 +1,64 @@
-import sys
+from .higher_order.higher_order import HigherOrder
+from .symbolic.symbolic import Symbolic
 
 
 def _parenp(p, s):
     return "(" + s + ")" if p else s
 
 
-def lit(x):
-    def fmt(_):
-        return str(x)
+class Viewer(HigherOrder, Symbolic):
+    @staticmethod
+    def lit(x):
+        def fmt(_):
+            return str(x)
 
-    return fmt
+        return fmt
 
+    @staticmethod
+    def neg(x):
+        def fmt(p):
+            return _parenp(p > 3, "-" + x(3))
 
-def neg(x):
-    def fmt(p):
-        return _parenp(p > 3, "-" + x(3))
+        return fmt
 
-    return fmt
+    @staticmethod
+    def add(x, y):
+        def fmt(p):
+            return _parenp(p > 1, x(1) + " + " + y(1))
 
+        return fmt
 
-def add(x, y):
-    def fmt(p):
-        return _parenp(p > 1, x(1) + " + " + y(1))
+    @staticmethod
+    def mul(x, y):
+        def fmt(p):
+            return _parenp(p > 2, x(2) + " * " + y(2))
 
-    return fmt
+        return fmt
 
+    @staticmethod
+    def sym(x):
+        def fmt(_):
+            return x
 
-def sub(x, y):
-    return add(x, neg(y))
+        return fmt
 
+    @staticmethod
+    def lam(f):
+        body0 = f(lambda _: "DUMMY")(0)
+        var = "x" + str(body0.count("lambda"))
 
-def mul(x, y):
-    def fmt(p):
-        return _parenp(p > 2, x(2) + " * " + y(2))
+        def fmt(p):
+            return _parenp(p > 0, "lambda " + var + ": " + f(lambda _: var)(0))
 
-    return fmt
+        return fmt
 
+    @staticmethod
+    def app(f, x):
+        def fmt(p):
+            return _parenp(p > 4, f(4) + "(" + x(5) + ")")
 
-def sym(x):
-    def fmt(_):
-        return x
-
-    return fmt
-
-
-def lam(f):
-    body0 = f(lambda _: "DUMMY")(0)
-    var = "x" + str(body0.count("lambda"))
-
-    def fmt(p):
-        return _parenp(p > 0, "lambda " + var + ": " + f(lambda _: var)(0))
-
-    return fmt
-
-
-def app(f, x):
-    def fmt(p):
-        return _parenp(p > 4, f(4) + "(" + x(5) + ")")
-
-    return fmt
+        return fmt
 
 
 def view(x):
-    return x(sys.modules[__name__])(0)
+    return x(Viewer)(0)
