022900f40fb78a06afefcc66edd8b0d44e61f84f
Felix Thaler <thaler@cscs.ch>
2021-10-21 10:42:03 +0200

    Reverted push_negation style

--- src/lambda_calculus_dsl/transforms/push_negation.py
+++ src/lambda_calculus_dsl/transforms/push_negation.py
@@ -5,68 +5,57 @@ from typing import Any
 from .transform import Transform
 
 
-class Kind(enum.Enum):
-    UNKNOWN = enum.auto()
-    INVERTIBLE = enum.auto()
+@dataclass
+class Unknown:
+    value: Any
 
 
 @dataclass
-class Annotated:
-    kind: Kind
+class Invertible:
     value: Any
 
 
 class T(Transform):
     def fwd(self, x):
-        assert not isinstance(x, Annotated)
+        assert not isinstance(x, (Unknown, Invertible))
 
         def f(negate):
             assert not negate
             return x
 
-        return Annotated(kind=Kind.UNKNOWN, value=f)
+        return Unknown(f)
 
     def bwd(self, x):
-        assert isinstance(x, Annotated)
+        assert isinstance(x, (Unknown, Invertible))
         return x.value(False)
 
     def lit(self, x):
-        return Annotated(
-            kind=Kind.INVERTIBLE,
-            value=lambda negate: lambda s: s.lit(-x) if negate else s.lit(x),
+        return Invertible(
+            lambda negate: lambda s: s.lit(-x) if negate else s.lit(x),
         )
 
     def neg(self, x):
-        if x.kind == Kind.INVERTIBLE:
-            return Annotated(
-                kind=Kind.INVERTIBLE, value=lambda negate: x.value(not negate)
-            )
+        if isinstance(x, Invertible):
+            return Invertible(lambda negate: x.value(not negate))
         return super().neg(x)
 
     def add(self, x, y):
-        if x.kind == y.kind == Kind.INVERTIBLE:
-            return Annotated(
-                kind=Kind.INVERTIBLE,
-                value=lambda negate: lambda s: s.add(
-                    x.value(negate)(s), y.value(negate)(s)
-                ),
+        if isinstance(x, Invertible) and isinstance(y, Invertible):
+            return Invertible(
+                lambda negate: lambda s: s.add(x.value(negate)(s), y.value(negate)(s)),
             )
         return super().add(x, y)
 
     def mul(self, x, y):
-        if x.kind == y.kind == Kind.INVERTIBLE:
-            return Annotated(
-                kind=Kind.INVERTIBLE,
-                value=lambda negate: lambda s: s.mul(
-                    x.value(negate)(s), y.value(False)(s)
-                ),
+        if isinstance(x, Invertible) and isinstance(y, Invertible):
+            return Invertible(
+                lambda negate: lambda s: s.mul(x.value(negate)(s), y.value(False)(s)),
             )
         return super().mul(x, y)
 
     def sym(self, x):
-        return Annotated(
-            kind=Kind.INVERTIBLE,
-            value=lambda negate: lambda s: s.neg(s.sym(x)) if negate else s.sym(x),
+        return Invertible(
+            lambda negate: lambda s: s.neg(s.sym(x)) if negate else s.sym(x),
         )
 
 
