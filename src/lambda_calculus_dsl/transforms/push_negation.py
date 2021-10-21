905d315496f5ce79a4fcf1e90985da2e09c9f861
Felix Thaler <thaler@cscs.ch>
2021-10-21 09:37:46 +0200

    Alternative style for push_neg

--- src/lambda_calculus_dsl/transforms/push_negation.py
+++ src/lambda_calculus_dsl/transforms/push_negation.py
@@ -1,15 +1,18 @@
 from dataclasses import dataclass
+import enum
 from typing import Any
+
 from .transform import dummy_transform
 
 
-@dataclass
-class Unknown:
-    value: Any
+class Kind(enum.Enum):
+    UNKNOWN = enum.auto()
+    INVERTIBLE = enum.auto()
 
 
 @dataclass
-class Invertible:
+class Annotated:
+    kind: Kind
     value: Any
 
 
@@ -18,37 +21,47 @@ def fwd(x):
         assert not negate
         return x
 
-    return Unknown(value=f)
+    return Annotated(kind=Kind.Unknown, value=f)
 
 
 def bwd(x):
-    assert isinstance(x, (Unknown, Invertible))
+    assert isinstance(x, Annotated)
     return x.value(False)
 
 
 class T(dummy_transform(fwd, bwd)):
     def lit(self, x):
-        return Invertible(lambda negate: lambda s: s.lit(-x) if negate else s.lit(x))
+        return Annotated(
+            kind=Kind.INVERTIBLE,
+            value=lambda negate: lambda s: s.lit(-x) if negate else s.lit(x),
+        )
 
     def neg(self, x):
-        assert isinstance(x, Invertible)
-        return Invertible(lambda negate: x.value(not negate))
+        assert isinstance(x, Annotated) and x.kind == Kind.INVERTIBLE
+        return Annotated(kind=Kind.INVERTIBLE, value=lambda negate: x.value(not negate))
 
     def add(self, x, y):
-        assert isinstance(x, Invertible) and isinstance(y, Invertible)
-        return Invertible(
-            lambda negate: lambda s: s.add(x.value(negate)(s), y.value(negate)(s))
+        assert isinstance(x, Annotated) and x.kind == Kind.INVERTIBLE
+        assert isinstance(y, Annotated) and y.kind == Kind.INVERTIBLE
+        return Annotated(
+            kind=Kind.INVERTIBLE,
+            value=lambda negate: lambda s: s.add(
+                x.value(negate)(s), y.value(negate)(s)
+            ),
         )
 
     def mul(self, x, y):
-        assert isinstance(x, Invertible) and isinstance(y, Invertible)
-        return Invertible(
-            lambda negate: lambda s: s.mul(x.value(negate)(s), y.value(False)(s))
+        assert isinstance(x, Annotated) and x.kind == Kind.INVERTIBLE
+        assert isinstance(y, Annotated) and y.kind == Kind.INVERTIBLE
+        return Annotated(
+            kind=Kind.INVERTIBLE,
+            value=lambda negate: lambda s: s.mul(x.value(negate)(s), y.value(False)(s)),
         )
 
     def sym(self, x):
-        return Invertible(
-            lambda negate: lambda s: s.neg(s.sym(x)) if negate else s.sym(x)
+        return Annotated(
+            kind=Kind.INVERTIBLE,
+            value=lambda negate: lambda s: s.neg(s.sym(x)) if negate else s.sym(x),
         )
 
 
