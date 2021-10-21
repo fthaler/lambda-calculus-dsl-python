8b11b4d5262444a02a2c34217315bcac0a33bf14
Felix Thaler <thaler@cscs.ch>
2021-10-21 08:59:09 +0200

    Improved readability

--- src/lambda_calculus_dsl/viewer.py
+++ src/lambda_calculus_dsl/viewer.py
@@ -2,8 +2,8 @@ from .higher_order.higher_order import HigherOrder
 from .symbolic.symbolic import Symbolic
 
 
-def _parenp(p, s):
-    return "(" + s + ")" if p else s
+def _parenp(add_parens: bool, string: str):
+    return "(" + string + ")" if add_parens else string
 
 
 class Viewer(HigherOrder, Symbolic):
@@ -16,22 +16,22 @@ class Viewer(HigherOrder, Symbolic):
 
     @staticmethod
     def neg(x):
-        def fmt(p):
-            return _parenp(p > 3, "-" + x(3))
+        def fmt(precedence):
+            return _parenp(precedence > 3, "-" + x(3))
 
         return fmt
 
     @staticmethod
     def add(x, y):
-        def fmt(p):
-            return _parenp(p > 1, x(1) + " + " + y(1))
+        def fmt(precedence):
+            return _parenp(precedence > 1, x(1) + " + " + y(1))
 
         return fmt
 
     @staticmethod
     def mul(x, y):
-        def fmt(p):
-            return _parenp(p > 2, x(2) + " * " + y(2))
+        def fmt(precedence):
+            return _parenp(precedence > 2, x(2) + " * " + y(2))
 
         return fmt
 
@@ -47,15 +47,15 @@ class Viewer(HigherOrder, Symbolic):
         body0 = f(lambda _: "DUMMY")(0)
         var = "x" + str(body0.count("lambda"))
 
-        def fmt(p):
-            return _parenp(p > 0, "lambda " + var + ": " + f(lambda _: var)(0))
+        def fmt(precedence):
+            return _parenp(precedence > 0, "lambda " + var + ": " + f(lambda _: var)(0))
 
         return fmt
 
     @staticmethod
     def app(f, x):
-        def fmt(p):
-            return _parenp(p > 4, f(4) + "(" + x(5) + ")")
+        def fmt(precedence):
+            return _parenp(precedence > 4, f(4) + "(" + x(5) + ")")
 
         return fmt
 
